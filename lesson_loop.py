7db28801b3ea3d88595fef302a8e0bbb48578df0
ChungyiBossi <chungyi.bossi@gmail.com>
2023-07-04 21:04:10 +0800

    [refactor] autopep8 format

--- lesson_loop.py
+++ lesson_loop.py
@@ -1,3 +1,4 @@
+import numpy as np
 list_a = ['a', 'b', 'c', 'd', 'e']
 print(list_a)
 # replace with for-loop
@@ -11,24 +12,23 @@ print(list_a)
 #         print("Letter: ", letter)
 #         print("Letter upper: ", letter_upper.upper())
 #     print("------------------------------------------")
-    
+
 # enumerate
 # for index, letter in enumerate(list_a):
 #     index = index + 1
 #     print(index, ":", letter * index)
 
-import numpy as np
 list_n = np.random.randint(1, 1000, 30)
 
 list_even = list()
-list_odd  = list()
+list_odd = list()
 list_five = list()
 list_three_or_five = list()
 
 for number in list_n:
-    if number % 2 == 0: # even
+    if number % 2 == 0:  # even
         list_even.append(number)
-    else: # odd
+    else:  # odd
         list_odd.append(number)
 
     if number % 5 == 0:
@@ -39,9 +39,10 @@ for number in list_n:
 
 # comprehension
 list_even = [number for number in list_n if number % 2 == 0]
-list_odd  = [number for number in list_n if number % 2 == 1]
+list_odd = [number for number in list_n if number % 2 == 1]
 list_five = [number for number in list_n if number % 5 == 0]
-list_three_or_five = [number for number in list_n if number % 3 == 0  or number % 5 == 0]
+list_three_or_five = [number for number in list_n if number %
+                      3 == 0 or number % 5 == 0]
 print("list_even: ", list_even)
 print("list_odd: ", list_odd)
 print("list_five: ", list_five)
--- lesson_set.py
+++ lesson_set.py
@@ -1,5 +1,5 @@
-set_a = set([1,2,3,5,5,6,6])
-set_b = set([4,4,5,5,6,6])
+set_a = set([1, 2, 3, 5, 5, 6, 6])
+set_b = set([4, 4, 5, 5, 6, 6])
 print(f"Set a: {set_a}, Set b: {set_b}")
 print(set_a.difference(set_b))
 print(set_b.difference(set_a))
