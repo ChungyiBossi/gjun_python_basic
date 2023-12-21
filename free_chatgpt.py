8fedfe0dad97ed6072c0d7a1f27d1aac2a954d7d
ChungyiBossi <chungyi.bossi@gmail.com>
2023-12-21 21:04:51 +0800

    [feature] google login & gpt

---
+++ free_chatgpt.py
@@ -0,0 +1,55 @@
+from google_login import google_semi_auto_login
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import os, time, json
+
+def login_to_chatgpt(driver):
+    # step 3: login openai
+    driver.get('https://chat.openai.com/')
+    driver.find_element(By.XPATH, '//button[@data-testid="login-button"]').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//button[@class="cf8ab1d76 ca5439885 c90865442"]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//div[@class="lCoei YZVTmd SmR8"]').click()
+    time.sleep(5)
+    return driver
+
+def continuous_chat_wtih_chatgpt(driver):
+
+    # step 4: input text
+    conversation = list()
+    while True:
+        input_text = input("What you want to say?")
+        if input_text == 'quit':
+            break
+        textarea_element = driver.find_element(By.ID, 'prompt-textarea')
+        textarea_element.send_keys(input_text)
+        textarea_element.send_keys(Keys.RETURN)
+        time.sleep(10)
+        last_response = driver.find_elements(
+            By.XPATH,
+            '//div[@class="markdown prose w-full break-words dark:prose-invert light"]'
+        )[-1]
+        response_text_elements = last_response.find_elements(By.TAG_NAME, 'p')
+        content = list()
+        for p in response_text_elements:
+            content.append(p.text)
+        content = "".join(content)
+        conversation.append({
+            'user': input_text,
+            'system': content
+        })
+
+    return driver, conversation
+
+
+if __name__ == '__main__':
+    driver = google_semi_auto_login(os.getenv('Gmail'), os.getenv('Gmail_pwd'))
+    driver = login_to_chatgpt(driver)
+    driver, conversation = continuous_chat_wtih_chatgpt(driver)
+    with open(f'{time.time()}.json', 'w') as output:
+        conversation = json.dumps(conversation)
+        output.write(conversation)
+
+    driver.close()
--- google_login.py
+++ google_login.py
@@ -6,17 +6,17 @@ import undetected_chromedriver as uc
 import os
 import json
 
-def google_semi_auto_login():
+def google_semi_auto_login(gmail, password):
     # driver = webdriver.Chrome() # 普通的Chrome Driver會被google anti-bot service 阻擋
     driver = uc.Chrome( ) # 換成 undetected-chrome driver 
     driver.get("https://accounts.google.com/InteractiveLogin/identifier?continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%2Blogin%26rlz%3D1C1CHBF_zh-TWTW1057TW1057%26oq%3Dgoogle%2Blogi%26gs_lcrp%3DEgZjaHJvbWUqBwgAEAAYgAQyBwgAEAAYgAQyBwgBEAAYgAQyBggCEEUYOTIHCAMQABiABDIHCAQQABiABDIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPKgCALACAA%26pf%3Dcs%26sourceid%3Dchrome%26ie%3DUTF-8%26pli%3D1&ec=GAZAAQ&hl=zh-TW&passive=true&ifkv=ASKXGp2BnUYGlXLq6SGO4m_7WlldRUloGm0x1jur6uZRavNPytsn57fce4TwA3RGniY8oRt_zh5PYQ&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
     # step 1: google login
     time.sleep(3)
-    driver.find_element(By.ID, "identifierId").send_keys(os.getenv('Gmail'))
+    driver.find_element(By.ID, "identifierId").send_keys(gmail)
     time.sleep(2)
     driver.find_element(By.XPATH, "//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b']").click()
     time.sleep(5)
-    driver.find_element(By.XPATH, "//input[@type='password']").send_keys(os.getenv('Gmail_pwd'))
+    driver.find_element(By.XPATH, "//input[@type='password']").send_keys(password)
     driver.find_element(By.XPATH, "//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b']").click()
     
     time.sleep(3)
@@ -35,8 +35,8 @@ def start_chrome_with_profile(user_data_dir, profile_dir):
 
 if __name__ == '__main__':
 
-    # driver = google_semi_auto_login()
-    # driver.quit()
-    # time.sleep(5)
-    driver_2 = start_chrome_with_profile('chrome_profile', 'Default')
-    time.sleep(10)
+    driver = google_semi_auto_login(os.getenv('Gmail'), os.getenv('Gmail_pwd'))
+    driver.quit()
+    time.sleep(5)
+    # driver_2 = start_chrome_with_profile('chrome_profile', 'Default')
+    # time.sleep(10)
