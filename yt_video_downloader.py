ac330333bab698897b344f0a5080aa6826f3059d
ChungyiBossi <chungyi.bossi@gmail.com>
2023-12-23 18:55:08 +0800

    [feature] yt downloader update

--- yt_video_downloader.py
+++ yt_video_downloader.py
@@ -1,21 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pytube import YouTube
+from nltk.tokenize import RegexpTokenizer
 import time
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
-def download_yt_video(yt_url, video_folder_path):
+def download_yt_video(yt_url, video_folder_path, tokenizer=RegexpTokenizer(r'\w+')):
     yt = YouTube(yt_url)
     print(f'Download..., {yt.title}')
-    yt.streams.filter().get_audio_only().download(output_path=f'{video_folder_path}', filename=f"{yt.title}.mp3")
-    # 儲存為 mp3
-    print('ok!')
+
+    # 可以用nltk去除特殊符號
+    title = " ".join(tokenizer.tokenize(yt.title))
+    try:
+        yt.streams.filter().get_audio_only().download(
+            output_path=f'{video_folder_path}',
+            filename=f"{title}.mp3"
+        )
+        # 儲存為 mp3
+        print('ok!')
+    except Exception as e:
+        print('Something wrong:', e)
 
 
-if __name__ == '__main__':
-    driver = webdriver.Chrome()
-    driver.get("https://www.youtube.com/results?search_query=jay+chou")
+def search_and_download_yt_video(driver, keyword):
+    pattern = "+".join(keyword.split())
+
+    driver.get(f"https://www.youtube.com/results?search_query={pattern}")
     time.sleep(10)
 
     href_list = list()
@@ -23,10 +34,13 @@ if __name__ == '__main__':
     for element in video_elements:
         href = element.get_attribute('href')
         title = element.get_attribute('title')
-        if href and title and "周杰倫 Jay Chou" in title:
+        if href and title and keyword.lower() in title.lower():
             href_list.append(href)
-            print(title, href)
-            print()
 
     for href in href_list:
-        download_yt_video(href, 'yt_video_folder')
+        download_yt_video(href, f'yt_video_folder/{keyword}')
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    search_and_download_yt_video(driver, 'Yoasobi')
