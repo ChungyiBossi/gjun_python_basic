d292714906cb98bf3abbbd1aadff5825fc25d578
ChungyiBossi <chungyi.bossi@gmail.com>
2023-12-26 19:06:29 +0800

    [bugfix] language parameter name mising

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -4,31 +4,31 @@ from pydub import AudioSegment     # 載入 pydub 的 AudioSegment 模組
 
 def semgment_audio(mp3_path, output_folder_path='.', duration_ms=3*1000, target_format='wav'):
     song = AudioSegment.from_file(mp3_path)    # 讀取 mp3 檔案
-    for index in range(0, len(song), duration_ms): # 每30秒一個cut
+    for index in range(0, len(song), duration_ms): # 每3秒一個cut
         song[index: index + duration_ms].export(
-            f"{output_folder_path}/{mp3_path}_{index}.wav",
+            f"{output_folder_path}/{mp3_path}_{index}.{target_format}",
             format=target_format
         ) 
 
 def recognize_audio_file(wav_path, language='zh-TW'):
     r = sr.Recognizer()
+    text = ''
     with sr.AudioFile(wav_path) as source:
         r.adjust_for_ambient_noise(source)
         audio = r.record(source)  # read the entire audio file
         try:
-            text = r.recognize_google(audio, language)
+            text = r.recognize_google(audio, language=language)
             print("Google Speech Recognition thinks you said " + text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        
-    return text if text else None
-    
+    return text
+
 def recognize_voice_from_microphone(phrase_time_limit=0, language='zh-TW'):
     # 建立Recognizer物件
     r = sr.Recognizer()
-
+    text = ""
     # 開啟麥克風並進行錄音
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
@@ -48,3 +48,7 @@ def recognize_voice_from_microphone(phrase_time_limit=0, language='zh-TW'):
             print("無法連線至Google語音辨識服務：{0}".format(e))
             
     return text if text else None
+
+if __name__ == '__main__':
+    recognize_audio_file('River_South.mp3_63000.wav', 'zh-TW')
+
